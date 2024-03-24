a7cc42f777c0a8c0f2b0adc997b1b66e9c942aa8
afzal75 <afzalbhuiyan16@gmail.com>
2024-03-24 13:23:43 +0600

    code modified

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://post-digester-donation-frontend.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // MongoDB Connection URL
